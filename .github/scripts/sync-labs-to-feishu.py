033343ab9fb0c874601bd52885ac4de553f9d3e8
Hang <huhuhang@users.noreply.github.com>
2023-04-27 15:41:36 +0800

    fix bugs

--- .github/scripts/sync-labs-to-feishu.py
+++ .github/scripts/sync-labs-to-feishu.py
@@ -176,9 +176,9 @@ class Sync:
                 lab_skills.extend(skills)
                 skills_raw[f"step{i+1}"] = skills
         # Get record is from skills tree table
-        if len(lab_skills) > 0:
-            in_skills_tree = []
-            not_in_skills_tree = []
+        in_skills_tree = []
+        not_in_skills_tree = []
+        if len(lab_skills) > 0:         
             for skill in lab_skills:
                 skill_id = skills_dict.get(skill)
                 if skill_id != None:
