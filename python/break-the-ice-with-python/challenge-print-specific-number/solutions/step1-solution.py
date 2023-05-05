42f65121657ffbffb327b02c6548fd2c0b01b194
aden1350 <aden1350@gmail.com>
2023-05-05 18:57:33 +1000

    update solution

--- python/break-the-ice-with-python/challenge-print-specific-number/solutions/step1-solution.py
+++ python/break-the-ice-with-python/challenge-print-specific-number/solutions/step1-solution.py
@@ -1,6 +1,6 @@
-l=[]
-for i in range(2000, 3201):
-    if (i%7==0) and (i%5!=0):
-        l.append(str(i))
-
-print (','.join(l))
+def step1(start = 2000, end = 3201):
+    l=[]
+    for i in range(start, end):
+        if (i%7==0) and (i%5!=0):
+            l.append(str(i))
+    return (','.join(l))
