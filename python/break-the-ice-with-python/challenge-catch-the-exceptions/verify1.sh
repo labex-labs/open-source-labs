af94c34340b24cce40c6aecc9fca27e59720f0d8
胡舵 <81087363+huduo0812@users.noreply.github.com>
2023-05-08 18:48:31 +0800

    update

--- python/break-the-ice-with-python/challenge-catch-the-exceptions/verify1.sh
+++ python/break-the-ice-with-python/challenge-catch-the-exceptions/verify1.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
 cd /tmp && python3 catch_the_exceptions_test.py
-cat /home/labex/project/catch_the_exceptions.py | -E "try"
-cat /home/labex/project/catch_the_exceptions.py | -E "except"
+cat /home/labex/project/catch_the_exceptions.py | grep -E "try:"
+cat /home/labex/project/catch_the_exceptions.py | grep -E "except:"
