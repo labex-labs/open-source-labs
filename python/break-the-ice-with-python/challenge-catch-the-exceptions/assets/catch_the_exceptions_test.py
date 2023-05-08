00da1b97988c4a6e1ea6281312ccd026cc8f1ced
胡舵 <81087363+huduo0812@users.noreply.github.com>
2023-05-08 16:47:21 +0800

    Update catch_the_exceptions_test.py

--- python/break-the-ice-with-python/challenge-catch-the-exceptions/assets/catch_the_exceptions_test.py
+++ python/break-the-ice-with-python/challenge-catch-the-exceptions/assets/catch_the_exceptions_test.py
@@ -12,29 +12,22 @@ class TestDivideFunction(unittest.TestCase):
         with self.assertRaises(ZeroDivisionError):
             divide()
 
-    def check_try_except(filename):
-        with open('/home/labex/project/catch_the_exceptions.py', 'r') as f:
+    def test_try_except(self):
+        with open('/home/project/catch_the_exceptions.py', 'r') as f:
             source = f.read()
         tree = ast.parse(source)
-        divide()
         for node in ast.walk(tree):
             if isinstance(node, ast.Try):
                 if not node.handlers:
-                    print(
-                        f"Error: No exception handler in try-except block at line {node.lineno}")
+                    self.fail("No exception handler in try-except block at line {}".format(node.lineno))
                 elif len(node.handlers) > 1:
-                    print(
-                        f"Error: Multiple exception handlers in try-except block at line {node.lineno}")
+                    self.fail("Multiple exception handlers in try-except block at line {}".format(node.lineno))
                 elif not isinstance(node.handlers[0].type, ast.Name) or node.handlers[0].type.id != 'Exception':
-                    print(
-                        f"Error: Incorrect exception type in try-except block at line {node.lineno}")
+                    self.fail("Incorrect exception type in try-except block at line {}".format(node.lineno))
                 elif not node.handlers[0].body:
-                    print(
-                        f"Error: Empty exception handler in try-except block at line {node.lineno}")
+                    self.fail("Empty exception handler in try-except block at line {}".format(node.lineno))
                 else:
-                    print(
-                        f"Success: try-except block at line {node.lineno} is correctly formatted")
-
+                    self.assertTrue(True)
 
 if __name__ == '__main__':
     unittest.main(argv=['first-arg-is-ignored'], exit=False)
