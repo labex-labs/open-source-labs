0324ab98ffb9ae6fbe2b87187a62894795b86a0e
胡舵 <81087363+huduo0812@users.noreply.github.com>
2023-05-08 18:35:44 +0800

    update

--- python/break-the-ice-with-python/challenge-catch-the-exceptions/assets/catch_the_exceptions_test.py
+++ python/break-the-ice-with-python/challenge-catch-the-exceptions/assets/catch_the_exceptions_test.py
@@ -2,7 +2,7 @@ import unittest
 import ast
 import sys
 
-sys.path.append("/home/project")
+sys.path.append("/home/labex/project")
 
 from catch_the_exceptions import divide
 
@@ -11,6 +11,6 @@ class TestDivideFunction(unittest.TestCase):
     def test_divide_by_zero(self):
         with self.assertRaises(ZeroDivisionError):
             divide()
-            
+
 if __name__ == '__main__':
     unittest.main()
--- python/break-the-ice-with-python/challenge-catch-the-exceptions/step1.md
+++ python/break-the-ice-with-python/challenge-catch-the-exceptions/step1.md
@@ -4,4 +4,4 @@ Write a function to compute 5/0 and use try/except to catch the exceptions.
 
 ## Hints
 
-Use try/except to catch exceptions.
+- Use try/except to catch exceptions.
