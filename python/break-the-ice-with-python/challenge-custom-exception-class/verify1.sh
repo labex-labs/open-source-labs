0f59522160e2265eef18de99f30d100abb67eacd
胡舵 <2496261750@qq.com>
2023-05-09 11:12:23 +0800

    更新 verify1.sh

--- python/break-the-ice-with-python/challenge-custom-exception-class/verify1.sh
+++ python/break-the-ice-with-python/challenge-custom-exception-class/verify1.sh
@@ -3,4 +3,4 @@
 cd /tmp                                                                                                           
 echo "123" | python3 custom_exception_class_test.py
 cat /home/labex/project/custom_exception_class.py | grep -E "try:"
-cat /home/labex/project/custom_exception_class.py | grep -E "except:"
+cat /home/labex/project/custom_exception_class.py | grep -E "except"
