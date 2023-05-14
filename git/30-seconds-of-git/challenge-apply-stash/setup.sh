89c9ea4982db43e14cfbf4a933b3354e075d5279
xiaoshengyunan <1797063828@qq.com>
2023-05-14 23:26:21 +0800

    fix

--- git/30-seconds-of-git/challenge-apply-stash/setup.sh
+++ git/30-seconds-of-git/challenge-apply-stash/setup.sh
@@ -1,12 +1,13 @@
 #!/bin/zsh
-git clone https://github.com/labex-labs/git-playground.git /home/labex/project
+git clone https://github.com/labex-labs/git-playground.git
 cd /home/labex/project/git-playground
+git checkout -b feature-branch
+echo "some changes" >> README.md
+git stash save "my changes"
 git checkout master
 echo "some bug fixes" >> README.md
 git stash save "some bug fixes"
-git checkout -b feature-branch 
-echo "some changes" >> README.md
-git stash save "my changes"
+git checkout feature-branch
 git add .
 
 
