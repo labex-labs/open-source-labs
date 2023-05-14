ddf12db8fbd8cba744ab05126aa77eea0a7b9198
xiaoshengyunan <1797063828@qq.com>
2023-05-14 17:10:57 +0800

    fix

--- git/30-seconds-of-git/challenge-autocorrect/setup.sh
+++ git/30-seconds-of-git/challenge-autocorrect/setup.sh
@@ -1,1 +1,5 @@
 #!/bin/zsh
+git clone https://github.com/labex-labs/git-playground.git
+cd git-playground
+
+
--- git/30-seconds-of-git/challenge-autocorrect/verify1.sh
+++ git/30-seconds-of-git/challenge-autocorrect/verify1.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
-cat ~/.zsh_history | grep -v grep | grep "git config"
+cat ~/.zsh_history | grep -v grep | grep "git config --global help.autocorrect"
 cat ~/.zsh_history | grep -v grep | grep "git sttaus"
