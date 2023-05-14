65fa4fd582ec87443bd99bb86448d1b2bf32edad
xiaoshengyunan <1797063828@qq.com>
2023-05-14 17:41:20 +0800

    fix

--- git/30-seconds-of-git/lab-autocorrect/verify1.sh
+++ git/30-seconds-of-git/lab-autocorrect/verify1.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
-cat ~/.zsh_history | grep -v grep | grep "git config"
+cat ~/.zsh_history | grep -v grep | grep "git config --global help.autocorrect 1"
 cat ~/.zsh_history | grep -v grep | grep "git sttaus"
