de07bad8c49af1e35caaf54092545d999e8278d6
Hang <huhuhang@users.noreply.github.com>
2023-05-18 16:12:40 +0800

    fix workflow

--- .github/scripts/sync-labs-to-feishu.py
+++ .github/scripts/sync-labs-to-feishu.py
@@ -137,14 +137,24 @@ class Sync:
         """
         with open(file_path, "r") as f:
             index = json.load(f)
-        direction = re.compile(r"\.\/[a-z]+").findall(file_path)
+        direction = re.compile(r"\.\/[a-z1-9]+").findall(file_path)
         path_slug = file_path.removeprefix("./").removesuffix("/index.json")
         if len(direction) > 0:
             lab_direction = direction[0].replace("./", "").title()
-            if lab_direction == "Numpy":
-                lab_direction = "NumPy"
-            elif lab_direction == "Opencv":
-                lab_direction = "OpenCV"
+            directions_map = {
+                "Numpy": "NumPy",
+                "Opencv": "OpenCV",
+                "Css": "CSS",
+                "Html": "HTML",
+                "Mongodb": "MongoDB",
+                "Cpp": "C++",
+                "Mysql": "MySQL",
+                "Jquery": "jQuery",
+                "Javascript": "JavaScript",
+                "Es6": "ES6",
+            }
+            if lab_direction in directions_map.keys():
+                lab_direction = directions_map[lab_direction]
         else:
             lab_direction = None
         lab_title = index.get("title", None)
