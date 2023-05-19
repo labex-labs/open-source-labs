e1aa7d60bbd909ba581c5ec1c4bd5aef7f30cbc9
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-05-19 22:22:19 +0800

    Update verify1.sh

--- git/30-seconds-of-git/challenge-clone-repo/verify1.sh
+++ git/30-seconds-of-git/challenge-clone-repo/verify1.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
-(cd /home/labex/project/git-playground | git status | grep "On branch master") && (cd /home/labex/project/my-project | git status | grep "On branch master")
+(cd /home/labex/project/git-playground && git status | grep "On branch master" >/dev/null) && (cd /home/labex/project/my-project && git status | grep "On branch master" >/dev/null) && echo "True"
 
 
