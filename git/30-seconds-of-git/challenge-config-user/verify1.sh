a7df1fd498af3f90af66d4a8e4075c3a5a88fb41
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-05-20 20:32:19 +0800

    Update verify1.sh

--- git/30-seconds-of-git/challenge-config-user/verify1.sh
+++ git/30-seconds-of-git/challenge-config-user/verify1.sh
@@ -1,5 +1,4 @@
 #!/bin/zsh
 cd /home/labex/project/git-playground
-git config --list | grep "user.email=jane.doe@example.com
-user.name=Jane Doe"
+git config --list | grep "user.email=jane.doe@example.com" | grep "user.name=Jane Doe"
    
