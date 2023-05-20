d16f7760bc4e26b816aac5bbbd3ad4c2e3f647af
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-05-20 21:18:23 +0800

    Update verify1.sh

--- git/30-seconds-of-git/lab-config-user/verify1.sh
+++ git/30-seconds-of-git/lab-config-user/verify1.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-(cd /home/labex/project/git-playground && git config --list | grep " grep -q "user.email=jane.doe@example.com" >/dev/null) && (cd /home/labex/project/git-playground && git config --list | grep -q "user.name=Jane Doe" >/dev/null) && echo "True"
+(cd /home/labex/project/git-playground && git config --list | grep -q "user.email=jane.doe@example.com" >/dev/null) && (cd /home/labex/project/git-playground && git config --list | grep -q "user.name=Jane Doe" >/dev/null) && echo "True"
