0fee53ae3c238e9fba31ee4a0ba7a9ce343817e3
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-05-21 22:57:58 +0800

    Update verify1.sh

--- git/30-seconds-of-git/challenge-current-branch-name/verify1.sh
+++ git/30-seconds-of-git/challenge-current-branch-name/verify1.sh
@@ -1,4 +1,5 @@
 #!/bin/zsh
 cd /home/labex/project/git-playground
+git checkout feature-branch
 command=$(history | tail -n 3 | head -n 1);
 [ "${command#git rev-parse --abbrev-ref HEAD}" != "$command" ] && [ "$(git rev-parse --abbrev-ref HEAD)" = "feature-branch" ] && echo "True"
