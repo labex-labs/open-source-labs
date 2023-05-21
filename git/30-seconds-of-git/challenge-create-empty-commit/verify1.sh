5c27c0e73807807d5650107322b2283e7ca661d7
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-05-21 17:05:16 +0800

    Update verify1.sh

--- git/30-seconds-of-git/challenge-create-empty-commit/verify1.sh
+++ git/30-seconds-of-git/challenge-create-empty-commit/verify1.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
-(cd /home/labex/project/git-playground && git diff-tree --quiet HEAD~1 HEAD) && echo "True" || echo ""
+(cd /home/labex/project/git-playground && git diff-tree --quiet HEAD~1 HEAD) && (cd /home/labex/project/git-playground && git log --name-status HEAD^. .HEAD | grep -q "Empty commit") && echo "True" || echo ""
   
