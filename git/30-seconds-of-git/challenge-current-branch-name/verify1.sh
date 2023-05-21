80943d38bd60c964a7f6c4cd44616253403ab157
xiaoshengyunan <1797063828@qq.com>
2023-05-21 22:31:04 +0800

    fix

--- git/30-seconds-of-git/challenge-current-branch-name/verify1.sh
+++ git/30-seconds-of-git/challenge-current-branch-name/verify1.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
-cat ~/.zsh_history | grep -v grep | grep "git checkout"
-cat ~/.zsh_history | grep -v grep | grep "git rev-parse"
+command=$(history | tail -n 2 | head -n 1);
+[ "${command#git rev-parse --abbrev-ref HEAD}" != "$command" ] && [ "$(git rev-parse --abbrev-ref HEAD)" = "feature-branch" ] && echo "True"
