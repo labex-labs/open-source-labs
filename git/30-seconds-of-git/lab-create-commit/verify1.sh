0462e4ed1fd117380d49377a670a6e7dc9c135f2
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-05-21 23:40:55 +0800

    Update verify1.sh

--- git/30-seconds-of-git/lab-create-commit/verify1.sh
+++ git/30-seconds-of-git/lab-create-commit/verify1.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 cd /home/labex/project/git-playground
-(cat ~/.zsh_history | grep -v grep | grep "git rev-parse --abbrev-ref HEAD") && ($(git rev-parse --abbrev-ref HEAD)" = "feature-branch") && echo "True"
+(cat ~/.zsh_history | grep -v grep | grep "git rev-parse --abbrev-ref HEAD") && ($(git rev-parse --abbrev-ref HEAD) = "feature-branch") && echo "True"
