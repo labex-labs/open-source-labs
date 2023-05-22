055986ed403ecd04a62b8141d8f96dbde54eb532
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-05-22 17:17:37 +0800

    Update verify1.sh

--- git/30-seconds-of-git/challenge-delete-branch/verify1.sh
+++ git/30-seconds-of-git/challenge-delete-branch/verify1.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-(cd /home/labex/project/git-playground && cat ~/.zsh_history | grep -v grep | grep "git branch -b feature-1") && (cd /home/labex/project/git-playground && cat ~/.zsh_history | grep -v grep | grep "git branch -d feature-1") && (cd /home/labex/project/git-playground && ! git branch | grep -q "feature-1") && echo "True"
+(cd /home/labex/project/git-playground && cat ~/.zsh_history | grep -v grep | grep "git branch -b feature-1") && (cd /home/labex/project/git-playground && cat ~/.zsh_history | grep -v grep | grep "git branch -d feature-1") && (cd /home/labex/project/git-playground && (- (git branch | grep -q "feature-1"))) && echo "True"
