b527cacb0f3d2a9324bfc8704570c92f3ec83a98
xiaoshengyunan <1797063828@qq.com>
2023-05-24 07:26:13 +0800

    fix

--- git/30-seconds-of-git/challenge-delete-remote-branch/verify1.sh
+++ git/30-seconds-of-git/challenge-delete-remote-branch/verify1.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
-(cat ~/.zsh_history | grep -v grep | grep "git push -d origin feature-branch") && (cd /home/labex/project/git-playground && git branch -r | ! grep feature-branch) && echo "True"
+(cat ~/.zsh_history | grep -v grep | grep "git push -d origin feature-branch") && (cd /home/labex/project/git-playground && ! git branch -r | grep feature-branch) && echo "True"
 
