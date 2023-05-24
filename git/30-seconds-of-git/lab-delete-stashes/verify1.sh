16fa2f9a93a8d46b508275276c180def0dc6a902
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-05-24 08:43:40 +0800

    Update verify1.sh

--- git/30-seconds-of-git/lab-delete-stashes/verify1.sh
+++ git/30-seconds-of-git/lab-delete-stashes/verify1.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-(cat ~/.zsh_history | grep -v grep | grep "touch newfile.txt") && (cd /home/labex/project/git-playground && ! git stash list | grep "stash@") && echo "True"
+(cat ~/.zsh_history | grep -v grep | grep "touch newfile.txt") &&(cat ~/.zsh_history | grep -v grep | grep "git add") && (cat ~/.zsh_history | grep -v grep | grep "git stash save") && (cd /home/labex/project/git-playground && ! git stash list | grep "stash@") && echo "True"
