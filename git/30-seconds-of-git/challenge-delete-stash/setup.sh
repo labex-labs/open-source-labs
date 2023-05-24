4d6808813c5614812dabffabedb81e7b0c262973
huhuhang <huhuhang@users.noreply.github.com>
2023-05-24 09:06:24 +0000

    Formatting documents

--- git/30-seconds-of-git/challenge-delete-stash/setup.sh
+++ git/30-seconds-of-git/challenge-delete-stash/setup.sh
@@ -2,6 +2,6 @@
 cd /home/labex/project
 git clone https://github.com/labex-labs/git-playground
 cd git-playground
-echo "hello" > test.txt 
+echo "hello" > test.txt
 git add .
 git stash save "my stash"
