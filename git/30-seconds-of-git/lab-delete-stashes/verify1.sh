20e2a0dc1a011fff6fa42ee86161a5b45228f243
xiaoshengyunan <1797063828@qq.com>
2023-05-24 08:36:25 +0800

    fix

--- git/30-seconds-of-git/lab-delete-stashes/verify1.sh
+++ git/30-seconds-of-git/lab-delete-stashes/verify1.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-cat ~/.zsh_history | grep -v grep | grep "git stash"
+(cat ~/.zsh_history | grep -v grep | grep "touch newfile.txt") && (cd /home/labex/project/git-playground && ! git stash list | grep "stash@") && echo "True"
