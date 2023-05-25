5436f97d43f4c204d900cd9fd1d396107d589357
xiaoshengyunan <1797063828@qq.com>
2023-05-25 10:27:23 +0800

    fix

--- git/30-seconds-of-git/challenge-fetch-changes/verify1.sh
+++ git/30-seconds-of-git/challenge-fetch-changes/verify1.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-cat ~/.zsh_history | grep -v grep | grep "git fetch"
+(cat ~/.zsh_history | grep -v grep | grep "git fetch") && (cat ~/.zsh_history | grep -v grep | grep "cd git-playground") && echo "True"
