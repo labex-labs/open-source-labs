473824020c3f05ea6395cc505d54728971c6fc83
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-05-25 10:09:50 +0800

    Update verify1.sh

--- git/30-seconds-of-git/challenge-edit-config/verify1.sh
+++ git/30-seconds-of-git/challenge-edit-config/verify1.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-((cat ~/.zsh_history | grep -v grep | grep "git config --global -e") || (cat ~/.zsh_history | grep -v grep | grep "git configuration --global -e")) && echo "True"
+((cat ~/.zsh_history | grep -v grep | grep "git config --global -e") || (cat ~/.zsh_history | grep -v grep | grep "git configuration --global -e")) && (cat ~/.zsh_history | grep -v grep | grep "cd git-playground") && echo "True"
