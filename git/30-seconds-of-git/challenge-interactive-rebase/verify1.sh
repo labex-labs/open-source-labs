93f23c38dc15e871edb2375d01876c5660313514
xiaoshengyunan <1797063828@qq.com>
2023-05-30 09:58:34 +0800

    fix

--- git/30-seconds-of-git/challenge-interactive-rebase/verify1.sh
+++ git/30-seconds-of-git/challenge-interactive-rebase/verify1.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
-cat ~/.zsh_history | grep -v grep | grep "git rebase"
+(cat ~/.zsh_history | grep -v grep | grep "git rebase -i HEAD~3") && (cd /home/labex/project/git-playground && git branch -r | grep "my-branch") && echo "True"
     
