4aaca23317eeee6a959b30d7ce2179eabe7210c8
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-05-30 13:47:21 +0800

    Update verify1.sh

--- git/30-seconds-of-git/challenge-list-aliases/verify1.sh
+++ git/30-seconds-of-git/challenge-list-aliases/verify1.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-cd /home/labex/project/git-playground && history | grep "git config -l" | tail -n 1 | tee output.txt | awk '{$1=""; print $0}' | sh | grep -q "st=status" && grep -q "co=checkout" && grep -q "rb=rebase" && echo "True"
+cd /home/labex/project/git-playground && history | grep "git config -l" | tail -n 1 | tee output.txt | awk '{$1=""; print $0}' | sh | grep -q "co=checkout" && grep -q "st=status" && grep -q "rb=rebase" && echo "True"
