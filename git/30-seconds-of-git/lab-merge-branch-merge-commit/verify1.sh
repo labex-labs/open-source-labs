e554f625f13f344a014e015dcbfe32922c21bf74
xiaoshengyunan <1797063828@qq.com>
2023-05-31 21:08:03 +0800

    fix

--- git/30-seconds-of-git/lab-merge-branch-merge-commit/verify1.sh
+++ git/30-seconds-of-git/lab-merge-branch-merge-commit/verify1.sh
@@ -1,3 +1,2 @@
 #!/bin/zsh
-cat ~/.zsh_history | grep -v grep | grep "git checkout"
-cat ~/.zsh_history | grep -v grep | grep "git merge"
+(cat ~/.zsh_history | grep -v grep | grep "git merge --no-ff -m "Merge feature-branch" feature-branch") && (cd /home/labex/project/git-playground && git log | grep "Merge feature-branch") && echo "True"
