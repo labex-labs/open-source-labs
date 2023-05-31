4b646014ee584233677b45dc9267652add74c28f
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-05-31 19:09:52 +0800

    Update verify1.sh

--- git/30-seconds-of-git/challenge-merge-branch/verify1.sh
+++ git/30-seconds-of-git/challenge-merge-branch/verify1.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-cd /home/labex/project/git-playground && git log | grep "bbb"
+cd /home/labex/project/git-playground && git log --color=never | grep "HEAD -> master, origin/master, origin/HEAD, feature-branch-A"
