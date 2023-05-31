5431059cbb144ed117fb52437133810f63293260
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-05-31 20:58:38 +0800

    Update verify1.sh

--- git/30-seconds-of-git/challenge-merge-branch-merge-commit/verify1.sh
+++ git/30-seconds-of-git/challenge-merge-branch-merge-commit/verify1.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
-(cat ~/.zsh_history | grep -v grep | grep "git merge --no-ff -m "Merge feature-branch" feature-branch") && (cd /home/labex/project/git-playground && git log | grep "Merge feature-branch") && echo "True"
+(cat ~/.zsh_history | grep -v grep | grep 'git merge --no-ff -m "Merge feature-branch" feature-branch') && (cd /home/labex/project/git-playground && git log | grep "Merge feature-branch") && echo "True"
 
