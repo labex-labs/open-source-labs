392fa6286d95d246ffc0bbbd1e8f907625af9573
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-05-31 23:48:19 +0800

    Update verify1.sh

--- git/30-seconds-of-git/challenge-move-commits-to-branch/verify1.sh
+++ git/30-seconds-of-git/challenge-move-commits-to-branch/verify1.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
-(cat ~/.zsh_history | grep -v grep | grep "git reset HEAD~1 --hard") && (cat ~/.zsh_history | grep -v grep | grep "git cherry-pick master") && (cd /home/labex/project/git-playground && git log | grep "Add new feature to master branch") && echo "True"
+(cat ~/.zsh_history | grep -v grep | grep "git cherry-pick master") && (cd /home/labex/project/git-playground && git checkout feature-branch && git log | grep "Add new feature to master branch") && echo "True"
 
