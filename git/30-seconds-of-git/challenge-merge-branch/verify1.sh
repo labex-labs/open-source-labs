9243207ce6f176d92ef7198d043896b14fd43c43
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-05-31 18:51:39 +0800

    Update verify1.sh

--- git/30-seconds-of-git/challenge-merge-branch/verify1.sh
+++ git/30-seconds-of-git/challenge-merge-branch/verify1.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-(cd /home/labex/project/git-playground && git checkout master && git log | grep "(HEAD -> master, origin/master, origin/HEAD, feature-branch-A)") && echo "True"
+(cd /home/labex/project/git-playground && git log | grep "(HEAD -> master, origin/master, origin/HEAD, feature-branch-A)") && echo "True"
