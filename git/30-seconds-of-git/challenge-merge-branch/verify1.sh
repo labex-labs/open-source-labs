c56167b4f50e8445e827c84c388a518a755659df
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-05-31 19:28:59 +0800

    Update verify1.sh

--- git/30-seconds-of-git/challenge-merge-branch/verify1.sh
+++ git/30-seconds-of-git/challenge-merge-branch/verify1.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-cd /home/labex/project/git-playground && git log --color=never | grep "feature-branch-A"
+(cd /home/labex/project/git-playground && git log | grep "fix file2.txt") && (cat ~/.zsh_history | grep -v grep | grep "git merge feature-branch-A") && echo "True"
