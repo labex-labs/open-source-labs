9987ff2c7a081f33c5568a0736ff010384a29756
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-06-01 13:59:59 +0800

    Update verify1.sh

--- git/30-seconds-of-git/lab-move-commits-to-branch/verify1.sh
+++ git/30-seconds-of-git/lab-move-commits-to-branch/verify1.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
- (cat ~/.zsh_history | grep -v grep | grep "git reset HEAD~1 --hard") && (cd /home/labex/project/git-playground && git checkout feature-branch && git log | grep "Add new feature to master branch") && (cd /home/labex/project/git-playground && git checkout master && !git log | grep "Add new feature to master branch") && echo "True"
+ (cat ~/.zsh_history | grep -v grep | grep "git reset HEAD~1 --hard") && (cd /home/labex/project/git-playground && git checkout feature-branch && git log | grep "Add new feature to master branch") && (cd /home/labex/project/git-playground && git checkout master && ! git log | grep "Add new feature to master branch") && echo "True"
