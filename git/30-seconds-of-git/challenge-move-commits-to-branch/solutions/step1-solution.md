9d52ef6cee3dd97bbb64ab6aa99affe2a82ccb3e
xiaoshengyunan <1797063828@qq.com>
2023-06-01 12:19:20 +0800

    fix

--- git/30-seconds-of-git/challenge-move-commits-to-branch/solutions/step1-solution.md
+++ git/30-seconds-of-git/challenge-move-commits-to-branch/solutions/step1-solution.md
@@ -11,7 +11,7 @@ git add .
 git commit -m "Add new feature to master branch"
 git branch feature-branch
 # `feature-branch` branch is created containing the commit "Add new feature to master branch"
+git reset HEAD~1 --hard # Remove the commit from `master`
 git checkout feature-branch
-git cherry-pick master
 git log # Verify that the changes are now in `feature-branch`
 ```
--- git/30-seconds-of-git/challenge-move-commits-to-branch/verify1.sh
+++ git/30-seconds-of-git/challenge-move-commits-to-branch/verify1.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
-(cat ~/.zsh_history | grep -v grep | grep "git cherry-pick master") && (cd /home/labex/project/git-playground && git checkout feature-branch && git log | grep "Add new feature to master branch") && echo "True"
+(cat ~/.zsh_history | grep -v grep | grep "git reset HEAD~1 --hard") && (cd /home/labex/project/git-playground && git checkout feature-branch && git log | grep "Add new feature to master branch") && (cd /home/labex/project/git-playground && git checkout master && ! git log | grep "Add new feature to master branch") echo "True"
 
