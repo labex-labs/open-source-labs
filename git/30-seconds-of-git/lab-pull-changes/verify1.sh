3c3cf73aff63dc54fee68066b4c69abb52197ba8
xiaoshengyunan <1797063828@qq.com>
2023-06-04 18:41:24 +0800

    fix

--- git/30-seconds-of-git/lab-pull-changes/verify1.sh
+++ git/30-seconds-of-git/lab-pull-changes/verify1.sh
@@ -1,3 +1,2 @@
 #!/bin/zsh
-cat ~/.zsh_history | grep -v grep | grep "git pull"
-cat ~/.zsh_history | grep -v grep | grep "git checkout"
+(cat ~/.zsh_history | grep -v grep | grep "git pull") && (cat ~/.zsh_history | grep -v grep | grep "cd git-playground") && echo "True"
