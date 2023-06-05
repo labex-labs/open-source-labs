4280e0e545409e13b60bd86fef97e741974be519
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-06-05 16:07:47 +0800

    Update verify1.sh

--- git/30-seconds-of-git/challenge-remove-file-from-commit/verify1.sh
+++ git/30-seconds-of-git/challenge-remove-file-from-commit/verify1.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
-(cat ~/.zsh_history | grep -v grep | grep 'git rm --cached "file1.txt"') && (cat ~/.zsh_history | grep -v grep | grep "git commit --amend") && (cd /home/labex/project/git-playground && git log | grep "add git-playground.txt") && echo "True"
+(cat ~/.zsh_history | grep -v grep | grep "git rm --cached file1.txt") && (cat ~/.zsh_history | grep -v grep | grep "git commit --amend") && (cd /home/labex/project/git-playground && git log | grep "add git-playground.txt") && echo "True"
 
