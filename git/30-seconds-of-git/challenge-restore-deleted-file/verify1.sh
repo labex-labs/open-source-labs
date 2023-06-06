3b3664dae347e56d4c588acdd7f77fbc24a59e45
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-06-06 08:53:04 +0800

    Update verify1.sh

--- git/30-seconds-of-git/challenge-restore-deleted-file/verify1.sh
+++ git/30-seconds-of-git/challenge-restore-deleted-file/verify1.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
-(cat ~/.zsh_history | grep -v grep | grep "rm example.txt") && (cd /home/labex/project/git-playground && git status | grep "new file: example.txt") && echo "True"
+(cat ~/.zsh_history | grep -v grep | grep "rm example.txt") && (cd /home/labex/project/git-playground && git status | grep --color=always -E "(new file: example.txt)") && echo "True"
 
