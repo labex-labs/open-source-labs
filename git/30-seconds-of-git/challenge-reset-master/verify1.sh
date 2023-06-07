ad26ef86251a6f3fc3a1b447207f72b5c3bccd2b
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-06-08 00:10:56 +0800

    Update verify1.sh

--- git/30-seconds-of-git/challenge-reset-master/verify1.sh
+++ git/30-seconds-of-git/challenge-reset-master/verify1.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
-(cat ~/.zsh_history | grep -v grep | grep "git fetch origin") && (cat ~/.zsh_history | grep -v grep | grep "git reset --hard origin/master") && (cd /home/labex/project/git-playground && git status | grep "Your branch is up to date with 'origin/master'") && echo "True"
+(cat ~/.zsh_history | grep -v grep | grep "git fetch origin") && (cat ~/.zsh_history | grep -v grep | grep "git reset --hard origin/master") && (cd /home/labex/project/git-playground && git status | grep "nothing to commit, working tree clean") && echo "True"
 
 
