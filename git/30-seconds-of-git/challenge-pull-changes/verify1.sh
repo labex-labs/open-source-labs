4ccf95876471c2bcae143e2a90eff1abc9544884
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-06-07 13:33:41 +0800

    Update verify1.sh

--- git/30-seconds-of-git/challenge-pull-changes/verify1.sh
+++ git/30-seconds-of-git/challenge-pull-changes/verify1.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
-(cat ~/.zsh_history | grep -v grep | grep "git pull") && (cd /home/labex/project/git-playground && git pull origin master | grep -v grep | grep " create mode 100644 file1.txt
+(cat ~/.zsh_history | grep -v grep | grep "git pull origin master") && (cd /home/labex/project/git-playground && git pull origin master | grep -v grep | grep " create mode 100644 file1.txt
  create mode 100644 file2.txt") && echo "True"
