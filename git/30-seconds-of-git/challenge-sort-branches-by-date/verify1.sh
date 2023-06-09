209424acb68a3305e29edd802835694cb680a5a0
xiaoshengyunan <1797063828@qq.com>
2023-06-10 01:37:54 +0800

    fix

--- git/30-seconds-of-git/challenge-sort-branches-by-date/verify1.sh
+++ git/30-seconds-of-git/challenge-sort-branches-by-date/verify1.sh
@@ -1,2 +1,3 @@
 #!/bin/zsh
-cat ~/.zsh_history | grep -v grep | grep "git branch"
+(cat ~/.zsh_history | grep -v grep | grep "git branch --sort=-committerdate") && (cat ~/.zsh_history | grep -v grep | grep "cd git-playground") && echo "True"
+
