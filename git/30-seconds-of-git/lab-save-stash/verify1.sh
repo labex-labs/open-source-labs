870f4975a0d0f57c9849fc0eecd060397e0feb08
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-06-11 21:03:50 +0800

    Update verify1.sh

--- git/30-seconds-of-git/lab-save-stash/verify1.sh
+++ git/30-seconds-of-git/lab-save-stash/verify1.sh
@@ -1,3 +1,2 @@
 #!/bin/zsh
-(cat ~/.zsh_history | grep -v grep | grep 'git stash save "My changes"') && (cd /home/labex/project/git-playground && git stash apply | grep "error: Your local changes to the following files would be overwritten by merge:
-        README.md") && echo "True"
+(cd /home/labex/project/git-playground && git stash list | grep "On feature: My changes") && (cat ~/.zsh_history | grep -v grep | grep "git stash apply") && echo "True"
