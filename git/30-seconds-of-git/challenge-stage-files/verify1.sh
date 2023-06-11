0ed71746907f354a7eb997c48495e8a7910d5f43
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-06-11 22:06:33 +0800

    Update verify1.sh

--- git/30-seconds-of-git/challenge-stage-files/verify1.sh
+++ git/30-seconds-of-git/challenge-stage-files/verify1.sh
@@ -1,3 +1,5 @@
 #!/bin/zsh
+cat ~/.zsh_history | grep -v grep | grep "git add index.html style.css"
 cat ~/.zsh_history | grep -v grep | grep "git add *.js"
 cat ~/.zsh_history | grep -v grep | grep "git add ."
+
