6ba13949a5e4c69858c2e34943f2ef4f3366b10f
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-06-11 21:02:36 +0800

    Update verify1.sh

--- git/30-seconds-of-git/challenge-save-stash/verify1.sh
+++ git/30-seconds-of-git/challenge-save-stash/verify1.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-cd /home/labex/project/git-playground && git stash apply | grep "error"
+(cd /home/labex/project/git-playground && git stash list | grep "On feature: My changes") && (cat ~/.zsh_history | grep -v grep | grep "git stash apply") && echo "True"
