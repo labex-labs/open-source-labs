52f7ec09030f3460e784693a5aef3af51a0d6c7b
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-06-11 22:17:33 +0800

    Update verify1.sh

--- git/30-seconds-of-git/challenge-stage-files/verify1.sh
+++ git/30-seconds-of-git/challenge-stage-files/verify1.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
-(cat ~/.zsh_history | grep -v grep | grep "git add index.html style.css")&&(cat ~/.zsh_history | grep -v grep | grep "git add *.js")&&(cat ~/.zsh_history | grep -v grep | grep "git add .")&& echo "True"
+(cat ~/.zsh_history | grep -v grep | grep "git add") && (cat ~/.zsh_history | grep -v grep | grep "cd git-playground") && echo "True"
 
