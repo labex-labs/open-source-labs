ccf54842103ccaf0a51ac462909c0e6a8db4c13f
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-06-11 20:44:11 +0800

    Update verify1.sh

--- git/30-seconds-of-git/challenge-save-stash/verify1.sh
+++ git/30-seconds-of-git/challenge-save-stash/verify1.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
-(cat ~/.zsh_history | grep -v grep | grep 'git stash save "My changes"') && (cd /home/labex/project/git-playground && git stash apply | grep "error: Your local changes to the following files would be overwritten by merge:
+(cd /home/labex/project/git-playground && git stash apply | grep "error: Your local changes to the following files would be overwritten by merge:
         README.md")
