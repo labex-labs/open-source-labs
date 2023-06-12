3bb2de9825c26abc5ceb55bd8d8ef2842e9f7926
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-06-12 10:07:37 +0800

    rename-remote-branch (#105)
    * fix
    
    * fix
    
    * Update step1.md
    
    * Update step1.md
    
    * Update step1.md
    
    * Update step1.md

--- git/30-seconds-of-git/challenge-rename-remote-branch/verify1.sh
+++ git/30-seconds-of-git/challenge-rename-remote-branch/verify1.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
-cat ~/.zsh_history | grep -v grep | grep "git push"
-cat ~/.zsh_history | grep -v grep | grep "git checkout"
-cat ~/.zsh_history | grep -v grep | grep "git branch"
+(cat ~/.zsh_history | grep -v grep | grep "git branch -m feature-1 new-feature-1") && (cat ~/.zsh_history | grep -v grep | grep "git push origin --delete feature-1") && (cat ~/.zsh_history | grep -v grep | grep "git push origin -u new-feature-1") && (cd /home/labex/project/git-playground && git branch -a | grep "remotes/origin/new-feature-1") && echo "True"
+
+
