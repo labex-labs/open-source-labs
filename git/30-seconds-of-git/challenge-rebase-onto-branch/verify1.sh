9b06db6bed0be260a9ffb82a894240e2851d1355
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-06-12 10:03:47 +0800

    rebase-onto-branch (#101)
    * fix
    
    * Update verify1.sh
    
    * fix
    
    * Update step1.md
    
    * Update step1.md
    
    * Update step1.md
    
    * Update step1.md

--- git/30-seconds-of-git/challenge-rebase-onto-branch/verify1.sh
+++ git/30-seconds-of-git/challenge-rebase-onto-branch/verify1.sh
@@ -1,4 +1,3 @@
 #!/bin/zsh
-cat ~/.zsh_history | grep -v grep | grep "git checkout"
-cat ~/.zsh_history | grep -v grep | grep "git fetch"
-cat ~/.zsh_history | grep -v grep | grep "git rebase"
+(cat ~/.zsh_history | grep -v grep | grep "git rebase feature-branch") && (cd /home/labex/project/git-playground && git checkout master && git log -r | grep "Added some changes to README.md") && echo "True"
+
