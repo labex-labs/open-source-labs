9ba5d0e7447cfe35d4176aef643982003e39b535
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-06-13 12:10:40 +0800

    Update verify1.sh

--- git/30-seconds-of-git/lab-set-text-editor/verify1.sh
+++ git/30-seconds-of-git/lab-set-text-editor/verify1.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-(cat ~/.zsh_history | grep -v grep | grep -w "git commit") && (cat ~/.zsh_history | grep -v grep | grep 'git config --global core.editor "vim"') && (cd /home/labex/project/git-playground && cat hello.txt | grep "Hello, Git") && (cd /home/labex/project/git-playground && git log | grep "Update hello.txt") && echo "True"
+(cd /home/labex/project/git-playground && git config --list | grep "core.editor=vim") && (cd /home/labex/project/git-playground && git log | grep "Update hello.txt") && echo "True"
