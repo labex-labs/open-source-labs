42d08a5f643ca93a7e184e0c0077cfb7404ed7e4
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-06-18 15:10:09 +0800

    Update verify1.sh

--- git/30-seconds-of-git/challenge-set-default-push-branch/verify1.sh
+++ git/30-seconds-of-git/challenge-set-default-push-branch/verify1.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-(cat ~/.zsh_history | grep -v grep | grep "git push -u") && (cat ~/.zsh_history | grep -v grep | grep "git config push.default current") && (cd /home/labex/project/git-playground && git log origin/my-branch | grep "Add hello.txt") && echo "True"
+(cd /home/labex/project/git-playground && git config push.default | grep "current") && (cd /home/labex/project/git-playground && git log origin/my-branch | grep "Add hello.txt") && echo "True"
