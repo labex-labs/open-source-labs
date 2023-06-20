08232369ae7f557ce2be6096590490c18aa0c841
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-06-20 23:53:21 +0800

    Update verify1.sh

--- git/30-seconds-of-git/challenge-remove-file-from-commit/verify1.sh
+++ git/30-seconds-of-git/challenge-remove-file-from-commit/verify1.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
- (cd /home/labex/project/git-playground && git show HEAD | less -R | grep "add git-playground.txt") && (cd /home/labex/project/git-playground && git show HEAD | less -R | ! grep "file1.txt") && echo "True"
+ (cd /home/labex/project/git-playground && git show HEAD | less -R | grep "add git-playground.txt") && (cd /home/labex/project/git-playground && ! git show HEAD | less -R | grep "file1.txt") && echo "True"
 
