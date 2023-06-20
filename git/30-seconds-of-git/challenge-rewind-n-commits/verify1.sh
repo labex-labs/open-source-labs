af736a568213f3ec6d5b113d270ec886440209d5
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-06-21 00:22:07 +0800

    Update verify1.sh

--- git/30-seconds-of-git/challenge-rewind-n-commits/verify1.sh
+++ git/30-seconds-of-git/challenge-rewind-n-commits/verify1.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
-(cd /home/labex/project/git-playground && git log origin/rewind-commits --oneline | ! grep "Added file2.txt")  && echo "True"
+(cd /home/labex/project/git-playground && ! git log origin/rewind-commits --oneline | grep "Added file2.txt")  && echo "True"
 
