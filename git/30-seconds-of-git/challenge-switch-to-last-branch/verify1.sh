6aeaaf7fd5f4cc9a9a00684449656d44e343f794
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-06-21 17:09:41 +0800

    Update verify1.sh

--- git/30-seconds-of-git/challenge-switch-to-last-branch/verify1.sh
+++ git/30-seconds-of-git/challenge-switch-to-last-branch/verify1.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-(cd /home/labex/project/git-playgrund && git branch | less -R | grep "* master") && echo "True"
+(cd /home/labex/project/git-playground && git branch | less -R | grep "* master") && echo "True"
