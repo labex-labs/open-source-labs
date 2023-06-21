b81d2d23e6c098b7715fe2d0cd939147f7c48819
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-06-21 17:10:05 +0800

    Update verify1.sh

--- git/30-seconds-of-git/lab-switch-to-last-branch/verify1.sh
+++ git/30-seconds-of-git/lab-switch-to-last-branch/verify1.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-(cd /home/labex/project/git-playgrund && git branch | less -R | grep "* master") && (cat ~/.zsh_history | grep -v grep | grep "git checkout") && echo "True"
+(cd /home/labex/project/git-playground && git branch | less -R | grep "* master") && (cat ~/.zsh_history | grep -v grep | grep "git checkout") && echo "True"
