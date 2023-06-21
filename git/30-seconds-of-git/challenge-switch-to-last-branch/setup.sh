3089633edb351925df1fdad335afecf76e33d875
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-06-21 17:08:02 +0800

    Update setup.sh

--- git/30-seconds-of-git/challenge-switch-to-last-branch/setup.sh
+++ git/30-seconds-of-git/challenge-switch-to-last-branch/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 cd /home/labex/project
 git clone https://github.com/labex-labs/git-playground.git
-cd /home/labex/project/git-playgrund
+cd /home/labex/project/git-playground
 git fetch origin
 git checkout -b feature-branch
