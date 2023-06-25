2cf9d83dc7a845e7e83a01829adbdc11d3c82ead
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-06-25 21:00:55 +0800

    Update setup.sh

--- git/30-seconds-of-git/challenge-view-all-branches/setup.sh
+++ git/30-seconds-of-git/challenge-view-all-branches/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 cd /home/labex/project
 git clone https://github.com/labex-labs/git-playground.git
+cd /home/labex/project/git-playground
 git checkout -b one-branch
 git checkout -b two-branch
 git checkout -b three-branch
