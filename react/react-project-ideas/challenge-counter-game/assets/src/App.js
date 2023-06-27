d7d0834aef6b0acb89c4d542008e6924710941bf
Lotus <xiaoqi_li@foxmail.com>
2023-06-27 18:51:17 +0800

    modify config

--- react/react-project-ideas/challenge-counter-game/assets/src/App.js
+++
@@ -1,7 +0,0 @@
-import HomePage from "./components/HomePage/HomePage";
-
-function App() {
-  return <HomePage />;
-}
-
-export default App;
--- react/react-project-ideas/challenge-drag-drop/setup.sh
+++ react/react-project-ideas/challenge-drag-drop/setup.sh
@@ -5,3 +5,5 @@ cd ~/project
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 
 sudo apt-get install -y nodejs
+
+unzip project.zip && mv project/* ./ && rm -rf project*
