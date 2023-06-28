be115819c18cd65eec881945deffb520f7daca7c
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-06-28 22:50:46 +0800

    Update verify1.sh

--- git/30-seconds-of-git/lab-view-changes-summary/verify1.sh
+++ git/30-seconds-of-git/lab-view-changes-summary/verify1.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
-cat ~/.zsh_history | grep -v grep | grep "git shortlog"
+(cat ~/.zsh_history | grep -v grep | grep ".HEAD") && (cat ~/.zsh_history | grep -v grep | grep "git shortlog") && echo "True"
 
