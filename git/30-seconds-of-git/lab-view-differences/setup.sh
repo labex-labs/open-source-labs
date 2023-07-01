37cbedb51b67db70929fbabb52d2edc899523330
xiaoshengyunan <1797063828@qq.com>
2023-07-01 18:50:37 +0800

    fix

--- git/30-seconds-of-git/lab-view-differences/setup.sh
+++ git/30-seconds-of-git/lab-view-differences/setup.sh
@@ -1,1 +1,7 @@
 #!/bin/zsh
+cd /home/labex/project
+git clone https://github.com/labex-labs/git-playground.git
+cd /home/labex/project/git-playgorund
+echo "hello,world" >> README.md
+git add README.md
+echo "hello,labex" >> file1.txt
