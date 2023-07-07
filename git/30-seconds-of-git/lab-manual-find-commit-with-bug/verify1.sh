e4ce4e3f7f4e6ae93a01dc8ed48389904fbb5f79
xiaoshengyunan <1797063828@qq.com>
2023-07-07 21:21:57 +0800

    fix

--- git/30-seconds-of-git/lab-manual-find-commit-with-bug/verify1.sh
+++ git/30-seconds-of-git/lab-manual-find-commit-with-bug/verify1.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-cat ~/.zsh_history | grep -v grep | grep "git bisect"
+(cat ~/.zsh_history | grep -v grep | grep "git bisect good") && (cat ~/.zsh_history | grep -v grep | grep "git bisect bad") && (cd /home/labex/project/git-playground && git bisect && git reflog | less -R | grep "checkout: moving from master to") && echo "True"
