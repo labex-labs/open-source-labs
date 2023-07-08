6b96e5e7a4dbb1df4e946ff3dc82eed149465d7c
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-07-08 22:35:05 +0800

    Update verify1.sh

--- git/30-seconds-of-git/challenge-list-aliases/verify1.sh
+++ git/30-seconds-of-git/challenge-list-aliases/verify1.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-(cat ~/.zsh_history | grep "git config") && (cat ~/.zsh_history | less -R | grep "alias\.") && echo "True"
+(cat ~/.zsh_history | grep "git config") && (cat ~/.zsh_history | less -R | grep "alias") && echo "True"
