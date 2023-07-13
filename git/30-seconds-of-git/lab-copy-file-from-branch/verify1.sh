915dc955d4abfcf8a2a097a6febc5aef121d93bc
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-07-13 20:59:54 +0800

    Update verify1.sh

--- git/30-seconds-of-git/lab-copy-file-from-branch/verify1.sh
+++ git/30-seconds-of-git/lab-copy-file-from-branch/verify1.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
-(cd /home/labex/project/git-playground && git checkout feature-1 && ls | grep "hello.txt") && (cd /home/labex/project/git-playground && git checkout feature-2 && ls | grep "hello.txt") && (cd /home/labex/project/git-playground && git checkout feature-1 && cat hello.txt | grep "hello,world") && (cd /home/labex/project/git-playground && git checkout feature-2 && cat hello.txt | grep "hello,world") && echo "True"
+(cd /home/labex/project/git-playground && git checkout feature-1 && cat hello.txt | grep "hello,world") && (cd /home/labex/project/git-playground && git checkout feature-2 && cat hello.txt | grep "hello,world") && echo "True"
 
