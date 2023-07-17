07c7b433fdaaff2bc6546becce1d57d7ee3a0fef
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-07-17 18:45:42 +0800

    Update setup.sh

--- git/30-seconds-of-git/challenge-discard-uncommitted/setup.sh
+++ git/30-seconds-of-git/challenge-discard-uncommitted/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 cd /home/labex/project
 git clone https://github.com/labex-labs/git-playground.git
-cd git-playground
+cd /home/labex/project/git-playground
 echo "hello,world" > hello.txt
 git add .
