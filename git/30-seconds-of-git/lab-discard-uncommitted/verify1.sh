89445399cba511c479d40b1c8801b1f407abc703
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-07-17 18:38:54 +0800

    Update verify1.sh

--- git/30-seconds-of-git/lab-discard-uncommitted/verify1.sh
+++ git/30-seconds-of-git/lab-discard-uncommitted/verify1.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-(cd /home/labex/project/git-playground && git status && grep "nothing to commit, working tree clean") && (cd /home/labex/project/git-playground && ! ls | grep "hello.txt") && echo "True"
+(cd /home/labex/project/git-playground && git status | grep "nothing to commit, working tree clean") && (cd /home/labex/project/git-playground && ! ls | grep "hello.txt") && echo "True"
