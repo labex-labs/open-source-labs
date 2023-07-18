96733f1f309556f660f5a87a18c04ade5a2a54d7
xiaoshengyunan <1797063828@qq.com>
2023-07-18 13:50:23 +0800

    fix

--- git/30-seconds-of-git/lab-add-submodule/verify1.sh
+++ git/30-seconds-of-git/lab-add-submodule/verify1.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
-cd /home/labex/project/my-project
-git submodule status | grep git-playground
+(cd /home/labex/project/my-project && git submodule status | grep git-playground) && echo "True"
+
