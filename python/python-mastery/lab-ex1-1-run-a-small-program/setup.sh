24c42d4ff4d780e6150e3b299e0110a5822272fb
Hang <huhuhang@users.noreply.github.com>
2023-07-20 15:49:26 +0800

    fix

--- python/python-mastery/lab-ex1-1-run-a-small-program/setup.sh
+++ python/python-mastery/lab-ex1-1-run-a-small-program/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
 wget https://cdn.jsdelivr.net/gh/labex-labs/common-scripts@master/python-shell-history/.setup-python-shell-history.sh && zsh .setup-python-shell-history.sh
-touch ~/projects/art.py
+touch ~/project/art.py
