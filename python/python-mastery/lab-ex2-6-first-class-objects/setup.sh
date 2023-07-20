b56cd8f1306a72f5daae17f81b4c66780d41148d
Hang <huhuhang@users.noreply.github.com>
2023-07-20 16:41:07 +0800

    fix

--- python/python-mastery/lab-ex2-6-first-class-objects/setup.sh
+++ python/python-mastery/lab-ex2-6-first-class-objects/setup.sh
@@ -1,3 +1,4 @@
 #!/bin/zsh
 
 wget https://cdn.jsdelivr.net/gh/labex-labs/common-scripts@master/python-shell-history/.setup-python-shell-history.sh && zsh .setup-python-shell-history.sh
+unzip ctabus.csv.zip && rm ctabus.csv.zip
