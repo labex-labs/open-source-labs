20fe04fd9610aafd19cad644148d8d47c01f5abc
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-07-21 14:33:01 +0800

    Challenge clone repo (#207)
    * fix
    
    * fix
    
    * fix
    
    * Update verify1.sh
    
    * Update verify1.sh
    
    * Update step1.md
    
    * fix
    
    * fix
    
    * Update verify1.sh
    
    * Update step1.md
    
    * fix

--- git/30-seconds-of-git/challenge-clone-repo/verify1.sh
+++ git/30-seconds-of-git/challenge-clone-repo/verify1.sh
@@ -1,2 +1,3 @@
 #!/bin/zsh
-(cd /home/labex/project/my-project && cat ~/.zsh_history | grep -v grep | grep "git clone") && (cd /home/labex/project/my-project && git status | grep "On branch master" > /dev/null) && echo "True"
+(cd /home/labex/project/my-project && git status | grep "On branch master") && echo "True"
+
