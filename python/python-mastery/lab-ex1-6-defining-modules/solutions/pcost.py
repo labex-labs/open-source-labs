bffbe17f5030ec4e2695f408ec298ecb0db7d7ee
胡舵 <2496261750@qq.com>
2023-07-21 18:15:33 +0800

    fix

--- python/python-mastery/lab-ex1-6-defining-modules/solutions/pcost.py
+++ python/python-mastery/lab-ex1-6-defining-modules/solutions/pcost.py
@@ -20,4 +20,4 @@ def portfolio_cost(filename):
 
 
 if __name__ == "__main__":
-    print(portfolio_cost("/home/labex/project/portfolio2.dat"))
+    print(portfolio_cost("/home/labex/project/portfolio.dat"))
--- python/python-mastery/lab-ex2-1-different-ways-of-representing-records/verify3.sh
+++ python/python-mastery/lab-ex2-1-different-ways-of-representing-records/verify3.sh
@@ -2,3 +2,4 @@
 
 cat /home/labex/project/readrides.py | grep 'open'
 cat /home/labex/project/readrides.py | grep '__main__'
+cat /home/labex/project/readrides.py | grep 'get_traced_memory'
