cb3aede51bff7d941239cd28f6bce049f45bd055
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-07-21 23:32:34 +0800

    Update verify1.sh

--- git/30-seconds-of-git/challenge-move-commits-to-branch/verify1.sh
+++ git/30-seconds-of-git/challenge-move-commits-to-branch/verify1.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-(cd /home/labex/project/git-playground && git checkout feature-branch && git log | grep "Added hello.txt") && (cd /home/labex/project/git-playground && git checkout master && ! git log | grep "Added hello.txt") echo "True"
+(cd /home/labex/project/git-playground && git checkout feature-branch && git log | grep "Added hello.txt") && (cd /home/labex/project/git-playground && git checkout master && ! git log | grep "Added hello.txt") && echo "True"
 
 
 
