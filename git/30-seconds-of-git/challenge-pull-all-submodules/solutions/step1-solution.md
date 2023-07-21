1483faca164fd3366a4f2ce3abad158d02d3987e
xiaoshengyunan <1797063828@qq.com>
2023-07-21 23:00:08 +0800

    fix

--- git/30-seconds-of-git/challenge-pull-all-submodules/solutions/step1-solution.md
+++ git/30-seconds-of-git/challenge-pull-all-submodules/solutions/step1-solution.md
@@ -3,6 +3,7 @@ git submodule update --recursive --remote
 ```
 
 ```shell
+cd git-playground
 git submodule update --recursive --remote
 # Pulls all submodules from their respective remotes  
 ```
--- git/30-seconds-of-git/lab-pull-all-submodules/verify1.sh
+++ git/30-seconds-of-git/lab-pull-all-submodules/verify1.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-(cat ~/.zsh_history | grep -v grep | grep "cd git-playground") && (cat ~/.zsh_history | grep -v grep | grep "git submodule update --recursive --remote") && echo "True"
+(cat ~/.zsh_history | grep -v grep | grep "cd git-playground") && (cat ~/.zsh_history | grep -v grep | grep "git submodule update") && (cat ~/.zsh_history | grep -v grep | grep "remote") && echo "True"
