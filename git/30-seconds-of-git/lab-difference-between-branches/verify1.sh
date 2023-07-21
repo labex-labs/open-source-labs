081d0a336fd02cdd6761cbb6d45ae95adb87a3c6
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-07-21 14:37:38 +0800

    Challenge difference between branches (#221)
    * fix
    
    * fix
    
    * Update index.json
    
    * Update index.json
    
    * Update index.json
    
    * Update index.json
    
    * Update index.json
    
    * Update index.json
    
    * Update step1.md
    
    * Update step1.md
    
    * fix
    
    * fix
    
    * fix

--- git/30-seconds-of-git/lab-difference-between-branches/verify1.sh
+++ git/30-seconds-of-git/lab-difference-between-branches/verify1.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-(cat ~/.zsh_history | grep -v grep | grep "git diff feature-1..feature-2") && (cd /home/labex/project/git-playground && git diff feature-1..feature-2 | grep "README.md") && (cd /home/labex/project/git-playground && git diff feature-1..feature-2 | grep "index.html") && echo "True"
+(cat ~/.zsh_history | grep -v grep | grep "git diff") &&(cat ~/.zsh_history | grep -v grep | grep "feature-1..feature-2")&& echo "True"
