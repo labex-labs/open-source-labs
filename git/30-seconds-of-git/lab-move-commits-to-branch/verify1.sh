5f3ecb2d5d31fc7baa59aa6193823b4ffd68242f
xiaoshengyunan <1797063828@qq.com>
2023-07-21 21:12:29 +0800

    fix

--- git/30-seconds-of-git/lab-move-commits-to-branch/verify1.sh
+++ git/30-seconds-of-git/lab-move-commits-to-branch/verify1.sh
@@ -1,2 +1,4 @@
 #!/bin/zsh
- (cat ~/.zsh_history | grep -v grep | grep "git reset HEAD~1 --hard") && (cd /home/labex/project/git-playground && git checkout feature-branch && git log | grep "Add new feature to master branch") && (cd /home/labex/project/git-playground && git checkout master && ! git log | grep "Add new feature to master branch") && echo "True"
+(cd /home/labex/project/git-playground && git checkout feature-branch && git log | grep "Added hello.txt") && (cd /home/labex/project/git-playground && git checkout master && ! git log | grep "Added hello.txt") echo "True"
+
+
