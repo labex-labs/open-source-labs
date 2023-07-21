a86143c37286b47f5e592c2a63bc0fa9de050753
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-07-21 14:36:31 +0800

    Challenge current branch name (#216)
    * fix
    
    * fix
    
    * Update verify1.sh
    
    * Update verify1.sh
    
    * Update verify1.sh
    
    * Update verify1.sh
    
    * Update verify1.sh
    
    * Update verify1.sh
    
    * fix
    
    * fix
    
    * Update verify1.sh
    
    * Update verify1.sh

--- git/30-seconds-of-git/challenge-current-branch-name/verify1.sh
+++ git/30-seconds-of-git/challenge-current-branch-name/verify1.sh
@@ -1,2 +1,3 @@
 #!/bin/zsh
-(cd /home/labex/project/git-playground && cat ~/.zsh_history | grep -v grep | grep "git rev-parse --abbrev-ref HEAD") && (cd /home/labex/project/git-playground && git rev-parse --abbrev-ref HEAD | grep "feature-branch") && echo "True"
+(((cd /home/labex/project/git-playground && cat ~/.zsh_history | grep -v grep | grep "git rev-parse") && (cd /home/labex/project/git-playground && cat ~/.zsh_history | grep -v grep | grep "abbrev-ref")) || ((cd /home/labex/project/git-playground && cat ~/.zsh_history | grep -v grep | grep "git branch") && (cd /home/labex/project/git-playground && cat ~/.zsh_history | grep -v grep | grep "show-current"))|| ((cd /home/labex/project/git-playground && cat ~/.zsh_history | grep -v grep | grep "git symbolic-ref") && (cd /home/labex/project/git-playground && cat ~/.zsh_history | grep -v grep | grep "short"))
+) && echo "True"
