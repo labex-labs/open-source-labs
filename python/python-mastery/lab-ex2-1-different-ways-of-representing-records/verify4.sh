60d77f179800c2357f84f9a52ae4495a23a18a69
胡舵 <2496261750@qq.com>
2023-07-24 09:20:17 +0800

    更新 verify4.sh

--- python/python-mastery/lab-ex2-1-different-ways-of-representing-records/verify4.sh
+++ python/python-mastery/lab-ex2-1-different-ways-of-representing-records/verify4.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
-rm readrides.py && touch readrides.py
-cat /home/labex/project/readrides.py | grep 'open'
 cat /home/labex/project/readrides.py | grep 'reader'
 cat /home/labex/project/readrides.py | grep 'start'
 cat /home/labex/project/readrides.py | grep 'get_traced_memory'
+cat /home/labex/project/readrides.py | grep 'class'
+cat /home/labex/project/readrides.py | grep '__init__'
