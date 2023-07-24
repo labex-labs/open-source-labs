ebfe7340cbed1f897a5126d38b3aa5a3c3b3c348
xiaoshengyunan <1797063828@qq.com>
2023-07-24 23:44:05 +0800

    fix

--- git/30-seconds-of-git/challenge-view-commits-summary-no-merges/verify1.sh
+++ git/30-seconds-of-git/challenge-view-commits-summary-no-merges/verify1.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-(cd /home/labex/project/git-playground && git log --oneline | less -R | grep "Merge branch 'feature1'") && (cat ~/.zsh_history | grep -v grep | grep "git log") && (cat ~/.zsh_history | grep -v grep | grep "no-merges") && echo "True"
+(cat ~/.zsh_history | grep -v grep | grep "git log") && (cat ~/.zsh_history | grep -v grep | grep "no-merges") && echo "True"
