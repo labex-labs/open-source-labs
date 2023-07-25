f122927d18512b677bf8dd62ea8470d8c650e03a
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-07-25 16:57:09 +0800

    Challenge remove file from commit (#239)
    * fix
    
    * Update verify1.sh
    
    * fix
    
    * Update step1.md
    
    * Update step1.md
    
    * Update step1.md
    
    * Update step1.md
    
    * fix
    
    * fix
    
    * fix
    
    * Update verify1.sh
    
    * Update verify1.sh
    
    * Update verify1.sh
    
    * Update verify1.sh
    
    * Update step1-solution.md
    
    * Update step1-solution.md
    
    * Update step1.md
    
    * Update step1.md
    
    * fix

--- git/30-seconds-of-git/challenge-remove-file-from-commit/verify1.sh
+++ git/30-seconds-of-git/challenge-remove-file-from-commit/verify1.sh
@@ -1,7 +1,4 @@
 #!/bin/zsh
- (cd /home/labex/project/git-playground && git show HEAD | less -R | grep "add git-playground.txt") && (cd /home/labex/project/git-playground && git show HEAD | less -R | grep "diff --git a/file1.txt b/file1.txt
-deleted file mode 100644
-index bfccc4a..0000000
---- a/file1.txt
-+++ /dev/null") && echo "True"
+(cd /home/labex/project/git-playground && ! git show HEAD | less -R | grep "diff") &&(cd /home/labex/project/git-playground && git show HEAD | less -R | grep "Added file2.txt") && echo "True"
+
 
