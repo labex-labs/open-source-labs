2889d7a0021320244c2df70d6b8f70435b6f9e36
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-07-25 17:02:15 +0800

    Challenge pull changes (#234)
    * fix
    
    * Update step1.md
    
    * fix
    
    * fix
    
    * fix
    
    * Update step1-solution.md
    
    * Update verify1.sh
    
    * fix

--- git/30-seconds-of-git/challenge-pull-changes/verify1.sh
+++ git/30-seconds-of-git/challenge-pull-changes/verify1.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
-(cat ~/.zsh_history | grep -v grep | grep "git pull origin master") && (cd /home/labex/project/git-playground && git pull origin master | grep -v grep | grep " create mode 100644 file1.txt
- create mode 100644 file2.txt") && echo "True"
+(cd /home/labex/project/git-playground && ls | grep "file2.txt")&& (cd /home/labex/project/git-playground && ls | grep "file1.txt") && echo "True"
+
