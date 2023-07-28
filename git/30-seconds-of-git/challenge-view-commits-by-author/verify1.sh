2dc71ee2826c9d4b8609d89788d6dcb0d4ecb5bb
xiaoshengyunan <1797063828@qq.com>
2023-07-28 18:32:13 +0800

    fix

--- git/30-seconds-of-git/challenge-view-commits-by-author/verify1.sh
+++ git/30-seconds-of-git/challenge-view-commits-by-author/verify1.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
-(cat ~/.zsh_history | grep -v grep | grep "git log") && (cat ~/.zsh_history | grep -v grep | grep 'author="huhuhang"') && echo "True"
+(cat ~/.zsh_history | grep -v grep | grep "git log") && (cat ~/.zsh_history | grep -v grep | grep 'author="Hang"') && echo "True"
 
