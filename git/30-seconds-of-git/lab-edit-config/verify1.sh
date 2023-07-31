16e3430462ea81d713258006f9610ce405be0e4c
xiaoshengyunan <1797063828@qq.com>
2023-07-31 11:07:51 +0800

    fix

--- git/30-seconds-of-git/lab-edit-config/verify1.sh
+++ git/30-seconds-of-git/lab-edit-config/verify1.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 (cd /home/labex/project/git-playground && git config --list | grep "user.name=labex_git
-user.email=labex_git@example.com") && echo "True"
+user.email=labex_git@example.com") && (cat ~/.zsh_history | grep -v grep | grep "git config") && echo "True"
