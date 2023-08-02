1f47d19434a9f2d444896e5eb0be54efe8d2d9d0
胡舵 <81087363+huduo0812@users.noreply.github.com>
2023-08-03 00:33:18 +0800

    fix 2_6

--- python/python-mastery/lab-ex2-6-first-class-objects/verify4.sh
+++ python/python-mastery/lab-ex2-6-first-class-objects/verify4.sh
@@ -1,7 +1,5 @@
 #!/bin/zsh
 
 cat /home/labex/project/colreader.py | grep 'collections'
-cat /home/labex/project/colreader.py | grep 'csv'
 cat /home/labex/project/colreader.py | grep 'defaultdict'
-cat /home/labex/project/colreader.py | grep 'intern'
-cat /home/labex/project/colreader.py | grep 'get_traced_memory'
+cat /home/labex/project/colreader.py | grep 'append'
