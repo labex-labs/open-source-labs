707850a18e489c29e1673317710188382da0c90b
胡舵 <81087363+huduo0812@users.noreply.github.com>
2023-08-03 15:04:01 +0800

    fix 3_3

--- python/python-mastery/lab-ex3-3-class-variables-and-class-methods/verify2.sh
+++ python/python-mastery/lab-ex3-3-class-variables-and-class-methods/verify2.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
-cat /home/labex/project/stock.py | grep '@classmethod'
+cat /home/labex/project/stock.py | grep -E '@.*classmethod'
 cat /home/labex/project/stock.py | grep 'zip'
--- python/python-mastery/lab-ex3-3-class-variables-and-class-methods/verify3.sh
+++ python/python-mastery/lab-ex3-3-class-variables-and-class-methods/verify3.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-cat ~/.python_history | grep "decimal"
-cat ~/.python_history | grep "class"
 cat ~/.python_history | grep "cost"
 cat ~/.python_history | grep "Stock"
+cat ~/.python_history | grep "decimal"
+cat ~/.python_history | grep "class"
