c8f34a0526210e919b88de70881e5cad112bbf78
胡舵 <81087363+huduo0812@users.noreply.github.com>
2023-08-03 16:50:50 +0800

    fix 3_8

--- python/python-mastery/lab-ex3-8-mixin-classes-and-cooperative-inheritance/verify2.sh
+++ python/python-mastery/lab-ex3-8-mixin-classes-and-cooperative-inheritance/verify2.sh
@@ -1,6 +1,4 @@
 #!/bin/zsh
 
-cat /home/labex/project/tableformat.py | grep 'zip'
-cat /home/labex/project/tableformat.py | grep 'super'
+cat /home/labex/project/tableformat.py | grep 'upper'
 cat ~/.python_history | grep "UpperHeadersMixin"
-cat ~/.python_history | grep "TextTableFormatter"
