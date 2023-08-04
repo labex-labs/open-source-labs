7300417cfa56d00832dc3062f93ea19cfc9900e6
胡舵 <81087363+huduo0812@users.noreply.github.com>
2023-08-04 15:50:12 +0800

    fix 4_3

--- python/python-mastery/lab-ex4-3-learn-about-descriptors/verify3.sh
+++ python/python-mastery/lab-ex4-3-learn-about-descriptors/verify3.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-ls /home/labex/project/validate.py | grep "__set__"
-ls /home/labex/project/stock.py | grep "String"
-ls /home/labex/project/stock.py | grep "PositiveInteger"
-ls /home/labex/project/stock.py | grep "PositiveFloat"
+cat /home/labex/project/validate.py | grep "__set__"
+cat /home/labex/project/stock.py | grep "String"
+cat /home/labex/project/stock.py | grep "PositiveInteger"
+cat /home/labex/project/stock.py | grep "PositiveFloat"
--- python/python-mastery/lab-ex4-3-learn-about-descriptors/verify4.sh
+++ python/python-mastery/lab-ex4-3-learn-about-descriptors/verify4.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-ls /home/labex/project/validate.py | grep "__set_name__"
-ls /home/labex/project/stock.py | grep -E "String.*\(\s*"
-ls /home/labex/project/stock.py | grep -E "PositiveInteger.*\(\s*"
-ls /home/labex/project/stock.py | grep -E "PositiveFloat.*\(\s*"
+cat /home/labex/project/validate.py | grep "__set_name__"
+cat /home/labex/project/stock.py | grep -E "String.*\(\s*"
+cat /home/labex/project/stock.py | grep -E "PositiveInteger.*\(\s*"
+cat /home/labex/project/stock.py | grep -E "PositiveFloat.*\(\s*"
