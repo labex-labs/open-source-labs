f3d2d0eb693f50a967fc8b8d8a8c44283a9dc589
胡舵 <81087363+huduo0812@users.noreply.github.com>
2023-08-04 17:28:24 +0800

    fix 5_1

--- python/python-mastery/lab-ex5-1-definitional-aspects-of-functions/verify3.sh
+++ python/python-mastery/lab-ex5-1-definitional-aspects-of-functions/verify3.sh
@@ -1,6 +1,3 @@
 #!/bin/zsh
 
 cat /home/labex/project/reader.py | grep "None"
-cat /home/labex/project/reader.py | grep "list"
-cat /home/labex/project/reader.py | grep "map"
-cat /home/labex/project/reader.py | grep "lambda"
