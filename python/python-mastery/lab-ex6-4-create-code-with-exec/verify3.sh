5ab4dc944e902cff3128de64c782fb23e278ef7a
胡舵 <81087363+huduo0812@users.noreply.github.com>
2023-08-05 16:10:52 +0800

    fix 6_4

--- python/python-mastery/lab-ex6-4-create-code-with-exec/verify3.sh
+++ python/python-mastery/lab-ex6-4-create-code-with-exec/verify3.sh
@@ -1,11 +1,5 @@
 #!/bin/zsh
 
-history_file="$HOME/.python_history"
-
-if [[ -f "$history_file" ]]; then
-    > "$history_file"
-fi
-
 cat ~/.python_history | grep "collections"
 cat ~/.python_history | grep "namedtuple"
 cat ~/.python_history | grep "inspect"
