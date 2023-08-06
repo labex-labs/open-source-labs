285528a75d82772eea67523c634abac3ce53600b
胡舵 <81087363+huduo0812@users.noreply.github.com>
2023-08-06 16:37:22 +0800

    fix 8_2

--- python/python-mastery/lab-ex8-1-customize-iteration-using-generators/verify4.sh
+++ python/python-mastery/lab-ex8-1-customize-iteration-using-generators/verify4.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 
+ls /home/labex/project/*.csv | grep -v "dowstocks.csv"
 grep "open" /home/labex/project/*.py | grep -v "structure.py" | grep -v "stocksim.py" | grep -v "validate.py" | grep -v "stock.py"
 grep "seek" /home/labex/project/*.py | grep -v "structure.py" | grep -v "stocksim.py" | grep -v "validate.py" | grep -v "stock.py"
 grep "SEEK_END" /home/labex/project/*.py | grep -v "structure.py" | grep -v "stocksim.py" | grep -v "validate.py" | grep -v "stock.py"
