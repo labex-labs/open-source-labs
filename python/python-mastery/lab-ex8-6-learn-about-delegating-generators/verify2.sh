543a7b9336ded3e5de9e439fc595ac61dd4453e0
胡舵 <81087363+huduo0812@users.noreply.github.com>
2023-08-06 23:57:37 +0800

    fix 8_6

--- python/python-mastery/lab-ex8-6-learn-about-delegating-generators/verify2.sh
+++ python/python-mastery/lab-ex8-6-learn-about-delegating-generators/verify2.sh
@@ -1,8 +1,3 @@
 #!/bin/zsh
 
-cat /home/labex/project/server.py | grep "class"
-cat /home/labex/project/server.py | grep "__init__"
-cat /home/labex/project/server.py | grep "__getattr__"
-cat /home/labex/project/server.py | grep "recv"
-cat /home/labex/project/server.py | grep "accept"
-cat /home/labex/project/server.py | grep "send"
+cat /home/labex/project/server.py | grep -E "yield.*from"
