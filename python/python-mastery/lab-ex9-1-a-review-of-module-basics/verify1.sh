53a6c53d2f756da0e578f112dfb841f0c3d7b8e5
胡舵 <81087363+huduo0812@users.noreply.github.com>
2023-08-07 00:11:46 +0800

    fix 9_1

--- python/python-mastery/lab-ex9-1-a-review-of-module-basics/verify1.sh
+++ python/python-mastery/lab-ex9-1-a-review-of-module-basics/verify1.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
-cat /home/labex/project/cofollow.py | grep "def"
-cat /home/labex/project/cofollow.py | grep "print"
-cat /home/labex/project/cofollow.py | grep "class"
+cat /home/labex/project/simplemod.py | grep "def"
+cat /home/labex/project/simplemod.py | grep "print"
+cat /home/labex/project/simplemod.py | grep "class"
--- python/python-mastery/lab-ex9-1-a-review-of-module-basics/verify4.sh
+++ python/python-mastery/lab-ex9-1-a-review-of-module-basics/verify4.sh
@@ -1,11 +1,5 @@
 #!/bin/zsh
 
-history_file="$HOME/.python_history"
-
-if [[ -f "$history_file" ]]; then
-    > "$history_file"
-fi
-
 cat ~/.python_history | grep  "from"
 cat ~/.python_history | grep  "simplemod"
 cat ~/.python_history | grep -E  "simplemod.*\..*\(.*\)"
