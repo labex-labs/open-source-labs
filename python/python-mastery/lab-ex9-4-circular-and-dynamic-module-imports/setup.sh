6dc66436a96922bdc2efce51e8b46ccd5304dc4f
胡舵 <81087363+huduo0812@users.noreply.github.com>
2023-08-07 01:14:44 +0800

    fix 9_4

--- python/python-mastery/lab-ex9-4-circular-and-dynamic-module-imports/setup.sh
+++ python/python-mastery/lab-ex9-4-circular-and-dynamic-module-imports/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
 wget https://cdn.jsdelivr.net/gh/labex-labs/common-scripts@master/python-shell-history/.setup-python-shell-history.sh && zsh .setup-python-shell-history.sh
-cd ~/project && unzip structly.zip && rm structly.zip
+cd ~/project && mkdir structly && unzip structly.zip -d ./structly && rm structly.zip
