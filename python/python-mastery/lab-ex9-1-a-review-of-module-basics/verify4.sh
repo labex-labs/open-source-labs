1ef2686a9b30f3f6a10e8de4b4503c01cd6a1546
胡舵 <81087363+huduo0812@users.noreply.github.com>
2023-08-07 00:53:19 +0800

    fix 9_3

--- python/python-mastery/lab-ex9-1-a-review-of-module-basics/verify4.sh
+++ python/python-mastery/lab-ex9-1-a-review-of-module-basics/verify4.sh
@@ -1,5 +1,3 @@
 #!/bin/zsh
 
 cat ~/.python_history | grep  "from"
-cat ~/.python_history | grep  "simplemod"
-cat ~/.python_history | grep -E  "simplemod.*\..*\(.*\)"
--- python/python-mastery/lab-ex9-3-controlling-symbols-and-combining-submodules/verify4.sh
+++ python/python-mastery/lab-ex9-3-controlling-symbols-and-combining-submodules/verify4.sh
@@ -3,4 +3,4 @@
 ls /home/labex/project/structly/tableformat
 ls /home/labex/project/structly/tableformat/__init__.py
 ls /home/labex/project/structly/tableformat/*/__init__.py
-cat /home/labex/project/structly/tableformat/*/*/*.py | grep -E "from.*\.\..*import"
+cat /home/labex/project/structly/tableformat/*/*.py | grep -E "from.*\.\..*import"
