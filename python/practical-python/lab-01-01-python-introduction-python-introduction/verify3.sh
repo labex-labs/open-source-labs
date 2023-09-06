56a1273712a26e73a3ed4ad756a4093f46284059
xiaoshengyunan <1797063828@qq.com>
2023-09-06 11:26:59 +0800

    fix

--- python/practical-python/lab-01-01-python-introduction-python-introduction/verify3.sh
+++ python/practical-python/lab-01-01-python-introduction-python-introduction/verify3.sh
@@ -1,1 +1,3 @@
 #!/bin/zsh
+(cat ~/.python_history | grep "print") && (cat ~/.python_history | grep "python") && echo "True"
+
--- python/practical-python/lab-01-01-python-introduction-python-introduction/verify6.sh
+++ python/practical-python/lab-01-01-python-introduction-python-introduction/verify6.sh
@@ -1,1 +1,3 @@
 #!/bin/zsh
+((cat ~/.python_history | grep "12 + 20") || (cat ~/.python_history | grep "(3 + 4
+         + 5 + 6)") || (cat ~/.python_history | grep "for i in range(5):)) && echo "True"
