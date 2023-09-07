961540012c2d08465b6bed4b3e1423db0ae1002c
xiaoshengyunan <1797063828@qq.com>
2023-09-07 18:32:53 +0800

    fix

--- python/practical-python/lab-01-06-files-file-management/verify5.sh
+++ python/practical-python/lab-01-06-files-file-management/verify5.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-(python3 ~/project/pcost.py > debug && grep "44671" debug) && echo "True"
+(python3 ~/project/pcost.py /home/labex/project/portfolio.csv > debug && grep "44671" debug) && echo "True"
