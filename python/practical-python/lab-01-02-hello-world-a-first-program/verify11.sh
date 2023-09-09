8cc1eabc92a61f804ee90ce1a5e9cb344e7beef7
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-09-10 00:16:29 +0800

    Update verify11.sh

--- python/practical-python/lab-01-02-hello-world-a-first-program/verify11.sh
+++ python/practical-python/lab-01-02-hello-world-a-first-program/verify11.sh
@@ -1,3 +1,2 @@
 #!/bin/zsh
-grep -q "while num_bills * bill_thickness < sears_height:
-    print" ~/.python_history && grep -q ":" ~/.python_history && echo "True"
+grep -q "while" ~/.python_history && grep -q "num_bills" ~/.python_history && grep -q ":" ~/.python_history && echo "True"
