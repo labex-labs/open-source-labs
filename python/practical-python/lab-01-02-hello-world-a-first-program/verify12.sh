8022780ca147a9f6bbebff7fbe6278c68412cd0e
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-09-10 00:07:59 +0800

    Update verify12.sh

--- python/practical-python/lab-01-02-hello-world-a-first-program/verify12.sh
+++ python/practical-python/lab-01-02-hello-world-a-first-program/verify12.sh
@@ -1,3 +1,2 @@
 #!/bin/zsh
-grep -q "while num_bills * bill_thickness < sears_height:
-print" ~/.python_history && grep -q ":" ~/.python_history && grep -q "print" ~/.python_history && echo "True"
+(grep -q "num_bills * bill_thickness" ~/.python_history || grep -q "num_bills*bill_thickness" ~/.python_history) && grep -q "print" ~/.python_history && echo "True"
