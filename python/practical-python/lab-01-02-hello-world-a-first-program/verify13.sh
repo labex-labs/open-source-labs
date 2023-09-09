6c155f0eccf6196dcdb977b07131e9ff3194803a
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-09-09 23:41:54 +0800

    Update verify13.sh

--- python/practical-python/lab-01-02-hello-world-a-first-program/verify13.sh
+++ python/practical-python/lab-01-02-hello-world-a-first-program/verify13.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-grep -q "while" ~/.python_history && grep -q ":" ~/.python_history && grep -q "    print" ~/.python_history && echo "True"
+grep -q "        day" ~/.python_history && grep -q "while" ~/.python_history && grep -q ":" ~/.python_history && grep -q "    print" ~/.python_history && echo "True"
