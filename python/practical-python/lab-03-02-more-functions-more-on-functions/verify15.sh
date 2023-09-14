b51d6b35bb5496c6fb97ef8622f3191b33b1995d
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-09-14 22:09:55 +0800

    Update verify15.sh

--- python/practical-python/lab-03-02-more-functions-more-on-functions/verify15.sh
+++ python/practical-python/lab-03-02-more-functions-more-on-functions/verify15.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-(python3 ~/project/fileparse_3.5.py > debug2 && grep "{'name': 'IBM', 'shares': 100, 'price': 70.44}" debug2) && echo "True"
+(python3 ~/project/fileparse_3.5.py > debug2 && grep "'shares': 100, 'price': 70.44" debug2) && echo "True"
