3c835d30ead218ccda4c817e7a850cdec6d19d0b
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-09-14 22:30:48 +0800

    Update verify17.sh

--- python/practical-python/lab-03-02-more-functions-more-on-functions/verify17.sh
+++ python/practical-python/lab-03-02-more-functions-more-on-functions/verify17.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-(python3 ~/project/fileparse_3.7.py > debug4 && grep "'shares': '95'" debug4) && echo "True"
+(python3 ~/project/fileparse_3.7.py > debug4 && grep "'shares': 95" debug4) && echo "True"
