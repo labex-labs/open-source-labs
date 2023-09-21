341b08bd67c2a6e626c1c997526493d162c71a9e
xiaoshengyunan <1797063828@qq.com>
2023-09-22 00:19:52 +0800

    fix

--- python/practical-python/lab-08-02-logging-logging/verify2.sh
+++ python/practical-python/lab-08-02-logging-logging/verify2.sh
@@ -1,1 +1,2 @@
 #!/bin/zsh
+(cat /home/labex/project/fileparse.py | grep -q "try") || (grep -q "try" ~/.python_history)
--- python/practical-python/lab-08-02-logging-logging/verify5.sh
+++ python/practical-python/lab-08-02-logging-logging/verify5.sh
@@ -1,1 +1,2 @@
 #!/bin/zsh
+(cat /home/labex/project/main.py | grep -q "__main__") || (grep -q "__main__" ~/.python_history)
