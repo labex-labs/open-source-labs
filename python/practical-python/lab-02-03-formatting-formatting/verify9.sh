b5d60d7a909a909d39b13aed35e258a5e3296983
xiaoshengyunan <1797063828@qq.com>
2023-09-27 22:21:21 +0800

    fix

--- python/practical-python/lab-02-03-formatting-formatting/verify9.sh
+++ python/practical-python/lab-02-03-formatting-formatting/verify9.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-(python3 ~/project/report.py > debug2  && ! cat ~/project/report.py | grep "f'$") && echo "True"
+python3 ~/project/report.py > debug2 && grep -q "\-----" debug2 && echo "True"
