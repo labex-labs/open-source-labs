4dfc3d4eb3f5eef59c1ef7ba97683bd5213d361b
xiaoshengyunan <1797063828@qq.com>
2023-10-01 16:59:36 +0800

    fix

--- python/practical-python/lab-03-05-main-module-main-module/assets/test_3.15.py
+++ python/practical-python/lab-03-05-main-module-main-module/assets/test_3.15.py
@@ -12,7 +12,7 @@ class TestScripts(unittest.TestCase):
         
         pcost_result = pcost.main(['/home/labex/project/pcost.py', '/home/labex/project/portfolio.csv'])
         pcost_expected_output = 'Total cost: 44671.15'
-        self.assertEqual(pcost_result.strip(), pcost_expected_output)
+        self.assertEqual(pcost_result, pcost_expected_output)
 
         
         report_result = report.main(['/home/labex/project/report.py', '/home/labex/project/portfolio.csv', '/home/labex/project/prices.csv'])
