2d142607f33616d3a74de85e04caad51830eb0da
xiaoshengyunan <1797063828@qq.com>
2023-10-03 23:32:01 +0800

    fix

--- python/practical-python/lab-07-03-returning-functions-returning-functions/assets/stock.py
+++ python/practical-python/lab-07-03-returning-functions-returning-functions/assets/stock.py
@@ -1,34 +1,45 @@
 # stock.py
 
-from typedproperty import String, Integer, Float
-
 
 class Stock:
     """
     An instance of a stock holding consisting of name, shares, and price.
     """
 
-    name = String("name")
-    shares = Integer("shares")
-    price = Float("price")
-
     def __init__(self, name, shares, price):
+        self._shares = None  # Private attribute to store the value of shares
         self.name = name
-        self.shares = shares
+        self.shares = shares  # Set the initial value using the property function
         self.price = price
 
     def __repr__(self):
         return f"Stock({self.name!r}, {self.shares!r}, {self.price!r})"
 
+
     @property
+    def shares(self):
+        """
+        Get the value of shares
+        """
+        return self._shares
+
+    @shares.setter
+    def shares(self, value):
+        """
+        Set the value of shares, ensuring it is an integer
+        """
+        if not isinstance(value, int):
+            raise TypeError("Expected an integer")
+        self._shares = value
+
     def cost(self):
         """
-        Return the cost as shares*price
+        Return the cost of the stock holding (shares * price)
         """
         return self.shares * self.price
 
     def sell(self, nshares):
         """
-        Sell a number of shares and return the remaining number.
+        Sell a number of shares
         """
         self.shares -= nshares
--- python/practical-python/lab-07-03-returning-functions-returning-functions/step9.md
+++
@@ -1,3 +0,0 @@
-# Exercise 7.9: Putting it into practice
-
-Rewrite the `Stock` class in the file `stock.py` so that it uses typed properties as shown.
