5ed6ded9d5dc1811b5ca351a73f73d6681a85e34
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-10-07 11:12:36 +0800

    lab-01-06-files-file-management (#302)
    * fix
    
    * fix
    
    * fix
    
    * Update step5.md
    
    * fix

--- python/practical-python/lab-01-06-files-file-management/verify4.sh
+++ python/practical-python/lab-01-06-files-file-management/verify4.sh
@@ -1,1 +1,2 @@
 #!/bin/zsh
+grep -q "print" ~/.python_history && grep -q "as" ~/.python_history && grep -q "open" ~/.python_history && grep -q "next" ~/.python_history && grep -q "split" ~/.python_history && grep -q "with" ~/.python_history && echo "True"
--- python/practical-python/lab-01-06-files-file-management/verify5.sh
+++ python/practical-python/lab-01-06-files-file-management/verify5.sh
@@ -1,1 +1,2 @@
 #!/bin/zsh
+python3 ~/project/pcost.py > debug && grep "44671" debug && echo "True"
