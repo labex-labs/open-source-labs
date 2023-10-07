28177eb416ceac1d683f9f88245ae6f2996c1a66
xiaoshengyunan <131872312+xiaoshengyunan@users.noreply.github.com>
2023-10-07 11:06:53 +0800

    lab-02-07-objects-objects (#314)
    * fix
    
    * fix

--- python/practical-python/lab-02-07-objects-objects/verify1.sh
+++ python/practical-python/lab-02-07-objects-objects/verify1.sh
@@ -1,1 +1,2 @@
 #!/bin/zsh
+grep -q "=" ~/.python_history && grep -q "append" ~/.python_history && echo "True"
--- python/practical-python/lab-02-07-objects-objects/verify7.sh
+++ python/practical-python/lab-02-07-objects-objects/verify7.sh
@@ -1,1 +1,2 @@
 #!/bin/zsh
+grep -q "import" ~/.python_history && grep -q "is" ~/.python_history && grep -q "deepcopy" ~/.python_history && echo "True"
