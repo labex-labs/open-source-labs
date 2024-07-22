c6464d9c92f910aab9e29105371f6d095053d616
huhuhang <no-reply@huhuhang.com>
2024-07-22 09:21:45 +0800

    fix verify

---
+++ git/30-seconds-of-git/lab-clone-repo/verify1-1.sh
@@ -0,0 +1,2 @@
+#!/bin/zsh
+cd /home/labex/project/git-playground && git status | grep "On branch master"
---
+++ git/30-seconds-of-git/lab-clone-repo/verify1-2.sh
@@ -0,0 +1,2 @@
+#!/bin/zsh
+cd /home/labex/project/my-project && git status | grep "On branch master"
