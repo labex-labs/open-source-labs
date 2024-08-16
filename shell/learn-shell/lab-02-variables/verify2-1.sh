95755aba27adcdbc0b9d7c040d2c07369e307e9f
Hang <huhuhang@users.noreply.github.com>
2024-08-16 10:36:31 +0800

    add lab-02-variables-v2

--- shell/learn-shell/lab-02-variables/verify2-1.sh
+++ shell/learn-shell/lab-02-variables/verify2-1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-grep -q 'echo "The price of an Apple today is: \$HK $PRICE_PER_APPLE"' /home/labex/project/variables.sh \
-  && grep -q 'echo "The first 10 letters in the alphabet are: ${MyFirstLetters}DEFGHIJ"' /home/labex/project/variables.sh \
-  && grep -q 'echo "$greeting"' /home/labex/project/variables.sh
+grep -q 'echo "price' /home/labex/project/variables.sh \
+  && grep -q 'echo "first' /home/labex/project/variables.sh \
+  && grep -q 'greeting' /home/labex/project/variables.sh
 if [ $? -eq 0 ]; then
   exit 0
 else
--- shell/learn-shell/lab-02-variables/verify4-2.sh
+++ shell/learn-shell/lab-02-variables/verify4-2.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-grep -q 'SUM=$((X + Y))' /home/labex/project/arithmetic.sh \
-  && grep -q 'DIFF=$((X - Y))' /home/labex/project/arithmetic.sh \
-  && grep -q 'PRODUCT=$((X * Y))' /home/labex/project/arithmetic.sh \
-  && grep -q 'QUOTIENT=$((X / Y))' /home/labex/project/arithmetic.sh \
-  && grep -q 'REMAINDER=$((X % Y))' /home/labex/project/arithmetic.sh \
-  && grep -q 'X=$((X + 1))' /home/labex/project/arithmetic.sh \
-  && grep -q 'Y=$((Y - 1))' /home/labex/project/arithmetic.sh
+grep -q 'SUM' /home/labex/project/arithmetic.sh \
+  && grep -q 'DIFF' /home/labex/project/arithmetic.sh \
+  && grep -q 'PRODUCT' /home/labex/project/arithmetic.sh \
+  && grep -q 'QUOTIENT' /home/labex/project/arithmetic.sh \
+  && grep -q 'REMAINDER' /home/labex/project/arithmetic.sh \
+  && grep -q 'X' /home/labex/project/arithmetic.sh \
+  && grep -q 'Y' /home/labex/project/arithmetic.sh
 if [ $? -eq 0 ]; then
   exit 0
 else
