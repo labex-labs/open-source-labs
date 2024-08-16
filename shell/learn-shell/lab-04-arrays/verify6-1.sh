45485a88a7326388a5446a9c39202482a77d9477
Hang <huhuhang@users.noreply.github.com>
2024-08-16 16:39:23 +0800

    add lab-04-arrays-v2

--- shell/learn-shell/lab-04-arrays/verify6-1.sh
+++ shell/learn-shell/lab-04-arrays/verify6-1.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-if grep -q 'echo "NUMBERS array: ${NUMBERS[@]}"' ~/project/arrays.sh \
-  && grep -q 'echo "The second name on the NAMES list is: $second_name"' ~/project/arrays.sh; then
+if grep -q 'echo' ~/project/arrays.sh; then
   exit 0
 else
   exit 1
