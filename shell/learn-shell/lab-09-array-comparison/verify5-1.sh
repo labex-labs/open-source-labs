f53d48224416eeed15af447d2d050b376293fcee
huhuhang <no-reply@huhuhang.com>
2024-08-19 11:13:00 +0800

    fix

--- shell/learn-shell/lab-09-array-comparison/verify5-1.sh
+++ shell/learn-shell/lab-09-array-comparison/verify5-1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 output=$(~/project/array-comparison.sh)
-if echo "$output" | grep -q "Common elements among a, b, and c:"; then
+if echo "$output" | grep -q "5"; then
   exit 0
 else
   exit 1
