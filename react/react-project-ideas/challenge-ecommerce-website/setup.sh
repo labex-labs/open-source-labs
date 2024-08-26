566667c138605a51e18c3866db8358f1c6dec42f
huhuhang <no-reply@huhuhang.com>
2024-08-26 08:42:07 +0800

    add apt-get update

--- react/react-project-ideas/challenge-ecommerce-website/setup.sh
+++ react/react-project-ideas/challenge-ecommerce-website/setup.sh
@@ -4,6 +4,7 @@ cd ~/project
 
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 
+sudo apt-get update
 sudo apt-get install -y nodejs
 
 unzip project.zip && mv project/* ./ && rm -rf project*
