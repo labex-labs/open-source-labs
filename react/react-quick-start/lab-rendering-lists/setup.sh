27843f469f12a02ac0b43fddec858110c3d45208
huhuhang <no-reply@huhuhang.com>
2024-10-21 14:21:16 +0800

    Update package installation commands in setup.sh

--- react/react-quick-start/lab-rendering-lists/setup.sh
+++ react/react-quick-start/lab-rendering-lists/setup.sh
@@ -2,7 +2,7 @@ cd ~/project
 
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 
-sudo apt-get update
-sudo apt-get install -y nodejs
+sudo apt update
+sudo apt install -y nodejs
 
 unzip demo.zip && mv demo/* ./ && rm -rf demo*
