9ab58241aa039921abd9b61f1a09bc2dd42a53bf
huhuhang <no-reply@huhuhang.com>
2025-01-22 16:35:44 +0800

    Update verification scripts to use absolute path for Go build command

--- go/go-by-example/challenge-command-line-arguments/verify1.sh
+++ go/go-by-example/challenge-command-line-arguments/verify1.sh
@@ -2,4 +2,4 @@
 # To experiment with command-line arguments it's best to
 # build a binary with `go build` first.
 cd /home/labex/project
-go build command-line-arguments.go | grep "cd /home/labex/project"
+/usr/local/go/bin/go build command-line-arguments.go | grep "cd /home/labex/project"
