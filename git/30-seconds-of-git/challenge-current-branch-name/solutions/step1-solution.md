0c8a099354867750bdbd34f03cf35516189e19bd
huhuhang <no-reply@huhuhang.com>
2025-02-24 09:37:14 +0800

    添加获取当前分支名称的多种方法

--- git/30-seconds-of-git/challenge-current-branch-name/solutions/step1-solution.md
+++ git/30-seconds-of-git/challenge-current-branch-name/solutions/step1-solution.md
@@ -1,10 +1,27 @@
 # Solutions
 
+Here are a few ways to get the current branch name:
+
+**Method 1: Using `git rev-parse`**
+
 ```shell
 git rev-parse --abbrev-ref HEAD
 ```
 
+**Method 2: Using `git symbolic-ref`**
+
 ```shell
-cd git-playground
-git rev-parse --abbrev-ref HEAD # Prints `master`
+git symbolic-ref --short HEAD
+```
+
+**Method 3: Using `git branch`**
+
+```shell
+git branch --show-current
+```
+
+**Method 4: Using `git status`**
+
+```shell
+git status | head -n 1 | awk '{print $3}'
 ```
--- git/30-seconds-of-git/challenge-current-branch-name/verify1-1.sh
+++ git/30-seconds-of-git/challenge-current-branch-name/verify1-1.sh
@@ -6,11 +6,13 @@ current_branch_rev_parse=$(cat ~/.zsh_history | grep -v grep | grep "git rev-par
 current_branch_abbrev_ref=$(cat ~/.zsh_history | grep -v grep | grep "abbrev-ref" | tail -n 1)
 current_branch_show_current=$(cat ~/.zsh_history | grep -v grep | grep "git branch" | grep "show-current" | tail -n 1)
 current_branch_symbolic_ref=$(cat ~/.zsh_history | grep -v grep | grep "git symbolic-ref" | grep "short" | tail -n 1)
+current_branch_status=$(cat ~/.zsh_history | grep -v grep | grep "git status" | tail -n 1)
 
 # Check if any of the methods returned a non-empty result
 if [ -n "$current_branch_rev_parse" ] \
   || [ -n "$current_branch_abbrev_ref" ] \
   || [ -n "$current_branch_show_current" ] \
-  || [ -n "$current_branch_symbolic_ref" ]; then
+  || [ -n "$current_branch_symbolic_ref" ] \
+  || [ -n "$current_branch_status" ]; then
   echo "True"
 fi
