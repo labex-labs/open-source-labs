1320d7d1a2ee8c3591de3ec382b5511d71dc37f5
huhuhang <no-reply@huhuhang.com>
2025-04-17 09:08:43 +0800

    Update verification script to check for "cost" method presence in stock.py, enhancing property validation logic.

--- python/python-mastery/lab-ex3-4-private-attributes-and-properties/verify2-1.sh
+++ python/python-mastery/lab-ex3-4-private-attributes-and-properties/verify2-1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /home/labex/project
-if grep -q "@property" stock.py && ! grep -q "def cost(self):" stock.py; then
+if grep -q "@property" stock.py && ! grep -q "cost" stock.py; then
   exit 0
 else
   exit 1
