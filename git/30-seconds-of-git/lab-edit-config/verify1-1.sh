8ccc6122212d388b9db9977e7ab7b8da76a3b066
huhuhang <huhuhang@users.noreply.github.com>
2025-05-26 14:39:59 +0800

    Update lab-edit-config to mark configuration as verified and enhance verification script to check for specific user details.

--- git/30-seconds-of-git/lab-edit-config/verify1-1.sh
+++ git/30-seconds-of-git/lab-edit-config/verify1-1.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-(cd /home/labex/project/git-playground && git config --list | grep "name") && echo "True"
+(cd /home/labex/project/git-playground && git config --list | grep "user.name=labex_git" && git config --list | grep "user.email=labex_git@example.com") && echo "True"
